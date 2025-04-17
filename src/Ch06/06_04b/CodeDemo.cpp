738b48493fa96048b2e565bcd63315518b85065c
IAmMinhAnh <nguyendangminhanh8@gmail.com>
2025-04-17 12:52:52 +0000

    practice put fuction

--- src/Ch06/06_04b/CodeDemo.cpp
+++ src/Ch06/06_04b/CodeDemo.cpp
@@ -5,12 +5,34 @@
 #include <iostream>
 #include <string>
 
-int main(){
-    std::string playerName = "Alex";
-    int score = 75;
+std::string NameHolder();
+int calculateScore(int x, int y);
+void displayScore(const std::string& name, int score);
 
-    std::cout << playerName << " scored " << score << " points." << std::endl;
-    
+int main(){
+    std::string name = NameHolder();
+
+    int score = calculateScore(75, 25);
+
+    displayScore(name, score);
+
     std::cout << std::endl << std::endl;
     return 0;
 }
+
+std::string NameHolder() {
+    std::string name;
+    std::cout << "What is your name? " << std::flush;
+    std::cin >> name;
+    return name;
+}
+
+int calculateScore(int x, int y) {
+    return x*y;
+}
+
+void displayScore(const std::string& name, int score) {
+    std::cout << name << " scored " << score << " points." << std::endl;
+}
+
+
